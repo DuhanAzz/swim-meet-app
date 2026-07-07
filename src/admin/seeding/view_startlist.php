9490dddf829233513985aff1c6a043b6cc5a0666
Duhan Muhammad Aziz <emailkamu@gmail.com>
2026-07-07 20:43:29 +0700

    fix: change loop type from for to foreach when rendering startlist rows

--- src/admin/seeding/view_startlist.php
+++ src/admin/seeding/view_startlist.php
@@ -340,7 +340,7 @@ foreach ($rawData as $row) {
                                     <td colspan="<?= ($activeColumnsCount - 1) ?>" style="color:#aaa; font-style:italic; padding-left:10px;">&lt;Kosong&gt;</td>
                                 <?php endif; ?>
                             </tr>
-                            <?php endfor; ?>
+                            <?php endforeach; ?>
                         </tbody>
                     </table>
                 <?php endforeach; ?>
